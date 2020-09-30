c174c4e406b88f88dbff82d514e04caef513a08e
elpeonkamilkaze <elpeonkamikaze@gmail.com>
2020-09-30 19:17:17 +0200

    times.c

--- times.c
+++ times.c
@@ -29,8 +29,11 @@ short average_sorting_time(pfunc_sort metodo,  int n_perms, int N, PTIME_AA ptim
   clock_t begin,end;
   double time=0;
  
-  array = generate_permutations(n_perms,N);
+  if(!metodo || !ptime) return ERR;
+
+  if(!(array = generate_permutations(n_perms,N))) return ERR;
   begin = clock();
+  //Insert Sort solo da ERROR si array[i] es NULL., pero ya hemos comprobado que no lo es antes
   a = InsertSort(array[i],0,N);
   end = clock();
   time += (end-begin)/CLOCKS_PER_SEC; 
@@ -49,7 +52,10 @@ short average_sorting_time(pfunc_sort metodo,  int n_perms, int N, PTIME_AA ptim
   ptime->max_ob = max;
   ptime->min_ob = min;
   ptime->N = N;
-  ptime->n_elems = 
+  ptime->average_ob = (double) count/n_perms;
+  ptime->n_elems = n_perms;
+  ptime->time = time/n_perms;
+  return OK;
 }
 
 
@@ -61,7 +67,21 @@ short average_sorting_time(pfunc_sort metodo,  int n_perms, int N, PTIME_AA ptim
 /***************************************************/
 short generate_sorting_times(pfunc_sort method, char* file, int num_min, int num_max, int incr, int n_perms)
 {
-  /* Your code */
+  int i;
+  short flag = OK;
+  PTIME_AA ptime;
+
+  if(!method || !file ) return ERR;
+
+  for(i=num_min;i<num_max && flag ==OK; i+= incr){
+    if(!(ptime = (PTIME_AA)malloc(sizeof(TIME_AA)))) flag = ERR;
+    if(flag != ERR) flag = average_sorting_time(method,n_perms,i,ptime);
+    if(flag != ERR) flag = save_time_table(file, ptime,n_perms);
+    free(ptime);
+  }
+
+  if(flag==ERR) return ERR;
+  return OK;
 }
 
 /***************************************************/
@@ -71,10 +91,9 @@ short generate_sorting_times(pfunc_sort method, char* file, int num_min, int num
 /***************************************************/
 short save_time_table(char* file, PTIME_AA ptime, int n_times)
 {
-  /* your code */
+  short flag = OK;
+  if(!file || !ptime) return ERR;
+  flag = fprintf(file,"|   %d   | %.lf |  %.lf   |   %d   |   %d   |   %d   |\n",ptime->N,ptime->time,ptime->average_ob,ptime->max_ob,ptime->min_ob,n_times);
+  fprintf(file,"----------------------------------o---O---o--------------------------------------------\n");
+  return flag;
 }
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 9891c41bff00f20e8cae6b3e3ac5a7b0fffdd0be

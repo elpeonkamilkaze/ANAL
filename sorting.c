abf50dbea1f232ae99863c3a9f3e3c88065a3774
Sergioleal14 <lealandressergio@gmail.com>
2020-10-12 10:29:49 +0200

    a

--- sorting.c
+++ sorting.c
@@ -19,22 +19,24 @@
 
 int InsertSort(int* table, int ip, int iu)
 {
-  int aux,i,j,cont=0;
+  int aux,i,j,cont=0, flag;
   if(!table||iu<0||ip<0) return ERR;
 
   for(i=ip+1;i<=iu;i++){
-
+    flag=1;
     aux=table[i];
     j=i-1;
     
-    for(;j>=ip && table[j]>aux;cont++,j--){
+    for(;j>=ip&&flag==1;j--){
+      cont++;
       
-
+      if(table[j]>aux){
       table[j+1]=table[j];
-      
-     
     }
-    if(j==i-1)cont++;
+    else flag=0;
+      
+    }
+    
     table[j+1]=aux;
     
   }
@@ -45,21 +47,23 @@ int InsertSort(int* table, int ip, int iu)
 
 int InsertSortInv(int* table, int ip, int iu)
 {
-  int aux,i,j,cont=0;
+  int aux,i,j,cont=0, flag;
   if(!table||iu<0||ip<0) return ERR;
 
   for(i=ip+1;i<=iu;i++){
-
+    flag=1;
     aux=table[i];
     j=i-1;
     
-    for(;j>=ip && table[j]<aux;cont++, j--){
+    for(;j>=ip && flag==1;cont++, j--){
+      cont++;
 
-      table[j+1]=table[j];
-      
-      
+      if(table[j]<aux) table[j+1]=table[j];
+
+      else flag=0;
+ 
     }
-    if(j==i-1)cont++;
+    
     table[j+1]=aux;
     
   }

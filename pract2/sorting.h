77888a96c9dc2c83b4b70b24718453dc74635971
elpeonkamilkaze <elpeonkamikaze@gmail.com>
2020-11-18 18:14:58 +0100

    todo

---
+++ pract2/sorting.h
@@ -0,0 +1,34 @@
+/**
+ *
+ * Descripcion: Header file for sorting functions 
+ *
+ * Fichero: sorting.h
+ * Autor: Carlos Aguirre
+ * Version: 1.0
+ * Fecha: 16-09-2019
+ *
+ */
+
+#ifndef ORDENACION_H
+#define ORDENACION_H
+#include <stdio.h>
+#include <stdlib.h>
+/* constants */
+
+#ifndef ERR
+  #define ERR -1
+  #define OK (!(ERR))
+#endif
+
+/* type definitions */
+typedef int (* pfunc_sort) (int *, int, int);
+typedef int (* pfunc_ordena)(int*, int, int);
+
+/* Functions */
+
+int InsertSort(int* table, int ip, int iu);
+int InsertSortInv(int* table, int ip, int iu);
+int MergeSort(int* table, int ip, int iu);
+int quicksort(int* table, int ip, int iu);
+int quicksortntr(int* table, int ip, int iu);
+#endif
